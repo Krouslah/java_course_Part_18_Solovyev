800122442e6856f09d710a45f2354552ee98dc08
Krouslah <xxxgunnerxxx@yandex.ru>
2022-02-13 11:47:00 +0300

    remove //

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         Road road = new Road();
         new Thread(new MyThread(road), "Thread 1").start();
         new Thread(new MySecondThread(road), "Thread 2").start();
-//        try {
-//            Thread.sleep(30000);
-//            System.exit(103);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Thread.sleep(30000);
+            System.exit(103);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
